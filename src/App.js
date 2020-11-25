b087b3804e00b3dbbdab22f2f68c41421566091e
JuanGrooth <christiangroothuis@pm.me>
2020-11-25 19:32:08 +0100

    fetch products and display them

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [items, setItems] = useState([]);
+	const [count, setCount] = useState(0);
+	useEffect(() => {
+		axios
+			.get(`http://127.0.0.1:8000/api/products/`)
+			.then((res) => {
+				setItems(res.data);
+				console.log(res);
+			})
+			.catch((err) => {
+				console.log(err.config);
+			});
+	}, [count]);
+	return (
+		<div className="App">
+      {count}
+			<button onClick={() => setCount(count + 1)}></button>
+			{items.map((item) => {
+				return (
+					<div>
+						{item.title} €{item.price}
+						{item.images[0] && (
+							<img
+                style={{width:"200px"}}
+								src={item.images[0].image_url}
+								alt={item.title}
+							></img>
+						)}
+            <p dangerouslySetInnerHTML={{__html: item.description}} />
+					</div>
+				);
+			})}
+		</div>
+	);
 }
 
 export default App;
